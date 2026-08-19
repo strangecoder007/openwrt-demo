a2e49282b8bedc0a441111266e571ae065410ad3
strangecoder007 <793013168@qq.com>
2026-08-19 21:28:39 +0800

    chore(miniprogram): bump version marker to v20260819.7

--- wechat-miniprogram/app.js
+++ wechat-miniprogram/app.js
@@ -1,7 +1,7 @@
 const auth = require('./utils/auth');
 
 App({
-  version: 'v20260819.6',
+  version: 'v20260819.7',
   globalData: { session: null },
   onLaunch() {
     this.globalData.session = auth.loadSession();
